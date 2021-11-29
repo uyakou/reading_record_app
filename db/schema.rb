c17c3956116f2122d79a417845a2556a4e3ed230
EC2 Default User <ec2-user@ip-172-31-14-207.ap-northeast-1.compute.internal>
2021-11-29 12:54:40 +0000

    Adjusting the view area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_095356) do
+ActiveRecord::Schema.define(version: 2021_11_29_080938) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_11_28_095356) do
     t.string "profile_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
