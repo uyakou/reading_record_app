e2600a37bdb8c95d1f0815746cb9248e442ac6d0
EC2 Default User <ec2-user@ip-172-31-14-207.ap-northeast-1.compute.internal>
2021-11-30 03:58:28 +0000

    Added easy login function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   attachment :profile_image
 
   validates :username, presence: true
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.username = "ゲスト"
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
   root to: "home#index"
   resources :users
   resources :books
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
 end
